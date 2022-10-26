ab3aed3227e83ce98367585b04d7f9b04bffb398
Orllewin <orllewin@fastmail.uk>
2022-10-26 13:14:54 +0100

    rename

--- test/dart_when_test.dart
+++ test/dart_when_test.dart
@@ -1,8 +1,9 @@
-import 'package:dart_when/dart_when.dart';
+import 'package:dart_when/when.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('basic when expression', () {
+    var i = when(50, {40: 40, 50: 50, 60: 60});
+    expect(i, 50);
   });
 }
